89dc1312c4b50ef0b3e55700ac6dc95bd2b18b15
eamendez85 <eamendez85@misena.edu.co>
2022-04-02 18:50:54 -0500

    Campo imagen agregado a libros

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def user_api_view(request):
+    if request.method == 'GET':
+        user=0
