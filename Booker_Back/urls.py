23b03170c3dfea11e635cbbfce1b250d3d715ce6
eamendez85 <eamendez85@misena.edu.co>
2022-04-18 13:40:03 -0500

    Login con token

--- Booker_Back/urls.py
+++ Booker_Back/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views.login_views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', Login.as_view(), name='Login'),
 ]
