858be67337f4d864f2468aeb2119e41668cddc5c
eamendez85 <eamendez85@misena.edu.co>
2022-04-19 11:52:58 -0500

    Proceso login Administrador_Usuario

--- api/views/login_views.py
+++ api/views/login_views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from api.models import Administradores, Estudiantes
+from api.serializers.usuarios_serializers import AdministradoresListSerializer, EstudiantesListSerializer
 from api.serializers.usuarios_serializers import UsuariosListSerializer
 
 class Login(ObtainAuthToken):
@@ -14,9 +16,15 @@ class Login(ObtainAuthToken):
         login_serializer = self.serializer_class(data = request.data, context = {'request':request})
         if login_serializer.is_valid():
             user = login_serializer.validated_data['user']
-            if user.is_active:
+            if user.usuario_activo:
                 token,created = Token.objects.get_or_create(user = user)
-                user_serializer = UsuariosListSerializer(user)
+                if user.usuario_administrador:
+                    administrador = Administradores.objects.filter(doc_administrador = user.doc).first()
+                    print("ADMINISTRADOR: ",administrador)
+                    user_serializer = AdministradoresListSerializer(administrador)
+                elif user.usuario_administrador==False:
+                    estudiante = Estudiantes.objects.filter(doc_estudiante = user.doc).first()
+                    user_serializer = EstudiantesListSerializer(estudiante)
                 if created:
                     return Response({
                         'token': token.key,
@@ -25,14 +33,14 @@ class Login(ObtainAuthToken):
                     }, status = status.HTTP_201_CREATED)
                 else:
                     #cerrar todas las sesiones al inicar sesion 34:41
+                    """
                     all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
                     if all_sessions.exists():
                         for session in all_sessions:
                             session_data = session.get_decoded()
                             if user.doc == int(session_data.get('_auth_user_id')):
                                 session.delete()
-                                
-                                
+                    """         
                     token.delete()
                     token = Token.objects.create(user = user)
                     return Response({
@@ -45,6 +53,5 @@ class Login(ObtainAuthToken):
             
         else:
             return Response({'error':'Nombre de usuario o contraseña incorrectos.'}, status = status.HTTP_400_BAD_REQUEST)
-        return Response({'mensaje':'Hola desde response'}, status = status.HTTP_200_OK)
             
         
