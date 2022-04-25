073f7f73a03b4f2be97e7ec1085e22a2334cfd66
kevina2208 <usamo489@gmail.com>
2022-04-25 11:44:38 -0500

    Correccion en serializer libros

--- api/serializers/general_serializers.py
+++ api/serializers/general_serializers.py
@@ -42,14 +42,10 @@ class IdiomasSerializer(serializers.ModelSerializer):
 #Serializer libros
 class LibrosSerializer(serializers.ModelSerializer):
 
-    categorias = CategoriasSerializer(many=True, read_only=True)
-    autores = AutoresSerializer(many=True, read_only=True)
-    id_editorial = serializers.StringRelatedField()
-    id_idioma = serializers.StringRelatedField()
 
     class Meta:
         model = Libros
-        fields = ['id_libro', 'isbn', 'imagen_libro', 'nombre', 'id_editorial', 'edicion','autores','id_idioma','categorias','descripcion', 'numero_paginas','alto','ancho','peso','presentacion','anexos','palabras_clave','estado']
+        fields = "__all__"
 
 
 
