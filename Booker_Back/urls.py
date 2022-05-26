122f8636e264a9b3f6cf6500d65ec0ca711dcb80
kevina2208 <usamo489@gmail.com>
2022-05-26 09:00:27 -0500

    Prueba 2 debug en true

--- Booker_Back/urls.py
+++ Booker_Back/urls.py
@@ -18,5 +18,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
