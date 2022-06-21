53e89898f9c175a547edb4b163deabb1ba20b03e
eamendez85 <eamendez85@misena.edu.co>
2022-06-21 09:09:25 -0500

    Cuando una infraccion pasa a completada el estado de de_prestamo_ pasa a AC

--- api/views/infracciones_views.py
+++ api/views/infracciones_views.py
@@ -14,7 +14,7 @@ class InfraccionesViewSet(viewsets.ModelViewSet):
     search_fields = ['id_bibliotecario__doc_bibliotecario__doc', 'id_estudiante__doc_estudiante__doc', 'ejemplares__id_libro__nombre', 'ejemplares__id_libro__isbn']
     ''' ordering_fields = ['ejemplares','id_tipo_infraccion','estado', 'id_bibliotecario', 'id_estudiante', 'ejemplares__id_libro']
  '''
-    serializer_class = InfraccionesListSerializer
+    serializer_class = InfraccionesSerializer
     def get_queryset(self, pk=None):
         if pk == None:
             return InfraccionesListSerializer.Meta.model.objects.all()
@@ -28,10 +28,13 @@ class InfraccionesViewSet(viewsets.ModelViewSet):
             if request.data['estado'] == "C":
                 prestamo_infraccion = infraccion.id_prestamo 
                 prestamo_infraccion.estado="C"
+                de_prestamo = prestamo_infraccion.id_de_prestamo
+                de_prestamo.estado = "AC"
                 ejemplar_prestamo = prestamo_infraccion.id_ejemplar
                 ejemplar_prestamo.estado="D"
                 prestamo_infraccion.save()
                 ejemplar_prestamo.save() 
+                de_prestamo.save()
             serializer.save()
             return Response({'data' : serializer.data, 'message':'Infracción actualizada correctamente'}, status= status.HTTP_200_OK)
         return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
